e8c61d51b83769314be90be7800f2a80e871a77d
yavor <ygk@mail.bg>
2024-02-13 16:30:14 +0200

    M-V-VM_Example

---
+++ app/src/main/java/com/example/mvvmodel_pattern_example/AppViewModel.java
@@ -0,0 +1,90 @@
+package com.example.mvvmodel_pattern_example;
+
+
+import android.text.TextUtils;
+import android.util.Patterns;
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+// This class will contain all the methods which are needed
+// to be called in the application layout.
+// The ViewModel class will extend BaseObservable
+// because it converts the data into streams
+// and notifies the View when the toast message property will change.
+public class AppViewModel extends BaseObservable {
+
+    // creating object of Model class
+    private Model model;
+
+    // string variables for
+    // toast messages
+    private String successMessage = "Login successful";
+    private String errorMessage = "Email or Password is not valid";
+
+    @Bindable
+    // string variable for
+    // toast message
+    private String toastMessage = null;
+
+    // getter and setter methods
+    // for toast message
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+    private void setToastMessage(String toastMessage) {
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    // getter and setter methods
+    // for email variable
+    @Bindable
+    public String getUserEmail() {
+        return model.getEmail();
+    }
+
+    public void setUserEmail(String email) {
+        model.setEmail(email);
+        notifyPropertyChanged(BR.userEmail);
+    }
+
+    // getter and setter methods
+    // for password variable
+    @Bindable
+    public String getUserPassword() {
+        return model.getPassword();
+    }
+
+    public void setUserPassword(String password) {
+        model.setPassword(password);
+        notifyPropertyChanged(BR.userPassword);
+    }
+
+    // constructor of ViewModel class
+    public AppViewModel() {
+
+        // instantiating object of
+        // model class
+        model = new Model("","");
+    }
+
+    // actions to be performed
+    // when user clicks
+    // the LOGIN button
+    public void onButtonClicked() {
+        if (isValid())
+            setToastMessage(successMessage);
+        else
+            setToastMessage(errorMessage);
+    }
+
+    // method to keep a check
+    // that variable fields must
+    // not be kept empty by user
+    public boolean isValid() {
+        return !TextUtils.isEmpty(getUserEmail()) && Patterns.EMAIL_ADDRESS.matcher(getUserEmail()).matches()
+                && getUserPassword().length() > 5;
+    }
+}
+
